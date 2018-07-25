b3f44fadb7a5b6693ed63a8e20190ab8906cf970
judy2254 <judyjaden225@icloud.com>
2018-07-25 18:52:14 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -6,10 +6,13 @@ solution = {}
  quarters = cents / 25
  solutio prices=prices=[:quarters] = num_quarters 
  
+num_dimes = cents/10 
+solution[:dimes]=num_dimes
+
 puts solution 
 
  end
  
- least_coins(50)
+ least_coins(70)
 
 
