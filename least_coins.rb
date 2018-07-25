6841ad2ba85990528d038ad0ae24dfd64edd46fd
judy2254 <judyjaden225@icloud.com>
2018-07-25 18:38:06 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,7 +2,7 @@
 
 def least_coins(cents)
 
-least_coins = {"quarters => 1","dimes => 0","nickles => 0","pennies => 4"}.
+least_coins = {"quarters => 1","dimes => 0","nickles => 0","pennies => 4"}
 
 least_coins[dimes] = 0
 
