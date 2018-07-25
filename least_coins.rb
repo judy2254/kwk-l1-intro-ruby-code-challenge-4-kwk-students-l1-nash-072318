9225a435d5a8ce2ecff80e17229b80056c808f44
judy2254 <judyjaden225@icloud.com>
2018-07-25 18:57:09 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,7 +4,7 @@ def least_coins(cents)
 solution = {}
  
  quarters = cents / 25
- solutio prices=prices=[:quarters] = num_quarters 
+ solution prices=prices=[:quarters] = num_quarters 
  
 num_dimes = cents/10 
 solution[:dimes]=num_dimes
