183ceb5df5d44401bc2c99da1adae603af7b7c64
judy2254 <judyjaden225@icloud.com>
2018-07-25 19:30:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -2,4 +2,10 @@
 shirts = ["tank top","t-shirt","button up"]
 pants = ["jeans","trousers"]
 jackets = ["jean","fur",]
-shoes = [tennis ]
+shoes = ["tennis","heels","boots" ]
+
+closte = ["shirts",pants jackets]
+pants << "levis"
+
+#closet closet = ["crew","ankle","fuzzy"]
+print closet 
