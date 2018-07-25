72aa8718431f7e52b0033968192f76a623614611
judy2254 <judyjaden225@icloud.com>
2018-07-25 19:09:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -12,7 +12,17 @@ num_dimes = cents/10
 solution[:dimes]=num_dimes
 # solution = {quarters => number of quarters in amount of cents, dimes => number of dimes in remaining amount of cents}
 
-cents = cents - 
+cents = cents - 10*num_dimes
+num_nikles = cents/5
+solution[:nikles]=num_nikles
+
+cents = cents - 5*num_nikles
+num_pennies = cents/1 
+solution[:pennies]=num_pennies
+
+puts solution
+
+
 
 
 
