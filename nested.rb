0cf725d42bbf4daa39a640656bb9478248c135af
judy2254 <judyjaden225@icloud.com>
2018-07-25 19:49:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -10,3 +10,10 @@ pants << "levis"
 #closet closet = ["crew","ankle","fuzzy"]
 print closet [0] [2]
 
+dressers = {
+  :tops => ["tank tops","graphic tee","dress shirt","blouses"] 
+  :bottoms => ["sweat pants", "shorts","skirts"]
+  :swim_suits => ["high waisted","bikini","one piece","tankini"]
+  
+}
+print dresser[:swim_suits] [2]
